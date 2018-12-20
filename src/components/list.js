d424ae50770f51dba92fd511cf8146e33eb2bbef
Jordan Salisbury <salisburyjordan21@gmail.com>
2018-12-19 18:05:39 -0800

    Progress on React to do list

--- src/components/list.js
+++ src/components/list.js
@@ -1,31 +1,15 @@
 import React, {Component} from 'react';
-import dummyList from '../data/to_do_list';
 
-
-
-class List extends Component{
-    state = {
-        list:[]
-    }
-    componentDidMount(){
-        this.getListData(); 
-    }
-    getListData(){
-        // Call server to get list data
-        this.setState({
-            list:dummyList
-        });
-    }
-    render(){
-        const listElements = this.state.list.map((item)=>{
-            return <li className="collection-item" key={item._id}>{item.title}</li>
-        });
-        return(
-                <ul className='collection'>
-                {listElements}
-                </ul>
-        )
-    }
+const List=(props)=>{
+    const listElements = props.toDos.map((item)=>{
+        return <li className="collection-item" key={item._id}>{item.title}</li>
+    });
+    return(
+            <ul className='collection'>
+            {listElements}
+            </ul>
+    )
 }
+
 
 export default List;
